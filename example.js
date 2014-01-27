9bc18f08651511f68472f81611742d4ca15697d9
Martin Drapeau <martindrapeau@gmail.com>
2014-01-27 15:08:05 -0500

    Adjusted indentation.

--- example.js
+++ example.js
@@ -7,87 +7,87 @@ $(document).ready(function() {
   });
   
   var columns = [{name: "id", label: "ID", cell: "integer", editable:false},
-	  {name: "name", label: "Name", cell: "string", editable:false},
-	  {name: "code", label: "Code", cell: "string", editable:false},
-	  {name: "continent_code", label: "Continent", cell: "string", editable:false}];
+    {name: "name", label: "Name", cell: "string", editable:false},
+    {name: "code", label: "Code", cell: "string", editable:false},
+    {name: "continent_code", label: "Continent", cell: "string", editable:false}];
   
   
   // ** Example 1 **
   (function(){
-	  var territories = new Territories(_.map(_territories, _.clone));
-	  
-	  // Grid
-	  var grid = new Backgrid.Grid({
-			columns: columns,
-			collection: territories,
-			className: "backgrid table"
-		  }),
-		  $grid = grid.render().$el;
-	  $("#result").append($grid);
+    var territories = new Territories(_.map(_territories, _.clone));
+    
+    // Grid
+    var grid = new Backgrid.Grid({
+      columns: columns,
+      collection: territories,
+      className: "backgrid table"
+      }),
+      $grid = grid.render().$el;
+    $("#result").append($grid);
 
-	  // Paginator
-	  var paginator = new Backgrid.Extension.Paginator({
-			collection: territories
-		  }),
-		  $paginator = paginator.render().$el;
-	  $("#result").append($paginator);
+    // Paginator
+    var paginator = new Backgrid.Extension.Paginator({
+      collection: territories
+      }),
+      $paginator = paginator.render().$el;
+    $("#result").append($paginator);
 
-	  // Select filter
-	  var filter = new Backgrid.Extension.SelectFilter({
-		  className: "backgrid-filter form-control",
-		  collection: territories,
-		  field: "continent_code",
-		  selectOptions: _.union([{label: "All", value: null}],
-			_.map(_continents, function(o) {return {label: o.name, value: o.code};}))
-		}),
-		$filter = filter.render().$el;
-	  $("#filter").replaceWith($filter);
+    // Select filter
+    var filter = new Backgrid.Extension.SelectFilter({
+      className: "backgrid-filter form-control",
+      collection: territories,
+      field: "continent_code",
+      selectOptions: _.union([{label: "All", value: null}],
+      _.map(_continents, function(o) {return {label: o.name, value: o.code};}))
+    }),
+    $filter = filter.render().$el;
+    $("#filter").replaceWith($filter);
   }).call(this);
   
   // ** Example 2 **
   (function(){
-	  var territories = new Territories(_.map(_territories, _.clone));
-	  
-	  // Grid
-	  var grid = new Backgrid.Grid({
-			columns: columns,
-			collection: territories,
-			className: "backgrid table"
-		  }),
-		  $grid = grid.render().$el;
-	  $("#result-2").append($grid);
+    var territories = new Territories(_.map(_territories, _.clone));
+    
+    // Grid
+    var grid = new Backgrid.Grid({
+      columns: columns,
+      collection: territories,
+      className: "backgrid table"
+      }),
+      $grid = grid.render().$el;
+    $("#result-2").append($grid);
 
-	  // Paginator
-	  var paginator = new Backgrid.Extension.Paginator({
-			collection: territories
-		  }),
-		  $paginator = paginator.render().$el;
-	  $("#result-2").append($paginator);
-	  
-	  // Select filter
-	  var filter = new Backgrid.Extension.SelectFilter({
-		    className: "backgrid-filter form-control",
-			collection: territories,
-			field: "continent_code",
-			selectOptions: [{
-			  label: "All", value: null
-			}, {
-			  label: "Americas", value: ["NA", "SA"]
-			}, {
-			  label: "Europe", value: ["EU"]
-			}, {
-			  label: "Asia", value: ["AS"]
-			}, {
-			  label: "Other", value: ["AF", "OC", "AN"]
-			}],
-			makeMatcher: function(value) {
-			  return function(model) {
-				return _.indexOf(value, model.get(this.field)) >= 0;
-			  }
-			}
-		  }),
-		  $filter = filter.render().$el;
-	  $("#filter-2").replaceWith($filter);
+    // Paginator
+    var paginator = new Backgrid.Extension.Paginator({
+      collection: territories
+      }),
+      $paginator = paginator.render().$el;
+    $("#result-2").append($paginator);
+    
+    // Select filter
+    var filter = new Backgrid.Extension.SelectFilter({
+        className: "backgrid-filter form-control",
+      collection: territories,
+      field: "continent_code",
+      selectOptions: [{
+        label: "All", value: null
+      }, {
+        label: "Americas", value: ["NA", "SA"]
+      }, {
+        label: "Europe", value: ["EU"]
+      }, {
+        label: "Asia", value: ["AS"]
+      }, {
+        label: "Other", value: ["AF", "OC", "AN"]
+      }],
+      makeMatcher: function(value) {
+        return function(model) {
+        return _.indexOf(value, model.get(this.field)) >= 0;
+        }
+      }
+      }),
+      $filter = filter.render().$el;
+    $("#filter-2").replaceWith($filter);
   }).call(this);
   
 });
